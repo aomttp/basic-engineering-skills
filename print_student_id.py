a5fe6d4508af540ab55d547b48c4bfc7914b072b
aomttp <124992535+aomttp@users.noreply.github.com>
2023-02-10 14:57:51 +0700

    add calculate sum function

--- print_student_id.py
+++ print_student_id.py
@@ -1,5 +1,9 @@
 def print_id(student_id)
     print("Student ID = ", student_id)
 
+def calculate_sum(array):
+    return array.sum()
+
 if __name__ == "__main__"
     print_id("34567")
+    calculate_sum(np.array([1, 2, 3]))
